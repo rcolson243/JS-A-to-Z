110d3c46de317998a4246b3051bef7657d4e6aa2
colson raphael <raphael.kasese@gmail.com>
2022-06-28 13:42:30 +0200

    cours js pour débutant

---
+++ cours/app.js
@@ -0,0 +1,17 @@
+const question_container = document.querySelector(".click-event")
+
+const reponse_un = document.getElementById('btn1');
+const reponse_deux = document.getElementById('btn2');
+const reponse_tois = document.getElementById('btn3');
+
+function hello_word () {
+    console.log("Salut toi comment tu vas");
+}
+
+question_container.addEventListener('click', () => {
+    console.log("click");
+})
+
+
+
+
