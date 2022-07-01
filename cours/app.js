7f750a469ab844019cc4e5920e1e18a0c332377f
colson raphael <raphael.kasese@gmail.com>
2022-07-01 16:06:11 +0200

    l'objet audio

--- cours/app.js
+++ cours/app.js
@@ -1,21 +1,49 @@
-const question_container = document.querySelector(".click-event")
+const question_container = document.querySelector('.click-event')
+const reponse_un         = document.getElementById('btn1');
+const reponse_deux       = document.getElementById('btn2');
+const bonne_reponse      = document.querySelector('.reponse')
 
-const reponse_un = document.getElementById('btn1');
-const reponse_deux = document.getElementById('btn2');
-const reponse_tois = document.getElementById('btn3');
+// click event
+reponse_un.addEventListener('click', () => {
+    bonne_reponse.classList.add('show-response');
+    bonne_reponse.style.backgroundColor = "#5bef76";
+})
+reponse_deux.addEventListener('click', () => {
+    bonne_reponse.classList.add('show-response');
+    bonne_reponse.style.backgroundColor = "red";
+})
 
-function hello_word () {
-    console.log("Salut toi comment tu vas");
+
+const mousemove = document.querySelector('.mousemove');
+
+window.addEventListener("mousemove", (e) => {
+    mousemove.style.left = e.pageX + "px";
+    mousemove.style.top  = e.pageY  + "px";
+    // mousemove.style.left = e.clientX + "px";
+    // mousemove.style.top = e.clientY  + "px";
+})
+
+const keypress_touch = document.querySelector('.keypress');
+const id_key         = document.getElementById('key');
+
+const son_music = () => {
+    const audio = new Audio();
+    audio.src = "./Enter.mp3";
+    audio.play();
 }
 
-question_container.addEventListener('click', () => {
-    console.log("click");
-    question_container.classList.toggle('question-clicked')
+document.addEventListener('keypress', (e) => {
+    id_key.textContent = e.key;
+    if (e.key == 'Enter') {
+        son_music();
+        console.log("hello");
+    }
 })
 
-document.querySelector(".cache").addEventListener('click', () => {
-    question_container.classList.remove('question-clicked')
-})
+
+
+
+
 
 
 
