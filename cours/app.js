7293a2a1573b5396ebdcff412731d8a5481ae251
colson raphael <raphael.kasese@gmail.com>
2022-07-01 12:01:32 +0200

    event

--- cours/app.js
+++ cours/app.js
@@ -1,16 +1,42 @@
 const question_container = document.querySelector(".click-event")
+const reponse_un         = document.getElementById('btn1');
+const reponse_deux       = document.getElementById('btn2');
+const bonne_reponse      = document.querySelector('.reponse')
 
-const reponse_un = document.getElementById('btn1');
-const reponse_deux = document.getElementById('btn2');
-const reponse_tois = document.getElementById('btn3');
 
-function hello_word () {
-    console.log("Salut toi comment tu vas");
-}
-
-question_container.addEventListener('click', () => {
-    console.log("click");
+// click event
+reponse_un.addEventListener('click', () => {
+    bonne_reponse.classList.add('show-response');
+    bonne_reponse.style.backgroundColor = "#5bef76";
 })
+reponse_deux.addEventListener('click', () => {
+    bonne_reponse.classList.add('show-response');
+    bonne_reponse.style.backgroundColor = "red";
+})
+
+// mouse event
+const mousemove = document.querySelector(".mousemove");
+// console.log(mousemove);
+
+window.addEventListener("mousemove", (e) => {
+    mousemove.style.left = e.pageX + "px";
+    mousemove.style.top = e.pageY  + "px";
+    // mousemove.style.left = e.clientX + "px";
+    // mousemove.style.top = e.clientY  + "px";
+})
+
+window.addEventListener("mousedown", (e)=> {
+    mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+})
+window.addEventListener("mouseup", (e)=> {
+    mousemove.style.transform = "scale(2) translate(-50%, -50%)";
+    mousemove.style.border = "3px solid red";
+})
+
+window.addEventListener("mouseenter", (e)=> {
+    ;
+})
+
 
 
 
